d6be4d30cf9623ceade697dd485f43fbd5381d9a
mogit2020 <mohibullah.kamal@gmail.com>
2020-11-12 22:52:19 +0100

    project files updated

--- api/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask_restful import Api
-from app import flaskAppInstance
-from .ProjectAPI import ProjectAPI
-
-
-
-restServerInstance = Api(flaskAppInstance)
-
-restServerInstance.add_resource(ProjectAPI,"/")
