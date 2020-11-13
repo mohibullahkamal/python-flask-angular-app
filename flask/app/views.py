f6c6aa3b262ab2eb7d09ae641aee184acbb1e791
mogit2020 <mohibullah.kamal@gmail.com>
2020-11-13 12:45:16 +0100

    fixed bug with Requirements.txt

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,5 +1,20 @@
+# from app import app
+#
+# @app.route("/")
+# def index():
+#     return "Hello from Flask.... You have successfully run a containerized Python-Flask app.... @Mohibullah Kamal"
+#
+
 from app import app
+import os
 
 @app.route("/")
 def index():
-    return "Hello from Flask.... You have successfully run a containerized Python-Flask app.... @Mohibullah Kamal"
+
+    # Use os.getenv("key") to get environment variables
+    app_name = os.getenv("APP_NAME")
+
+    if app_name:
+        return f"Hello from {app_name} running in a Docker container behind Nginx!.....@Mohibullah Kamal"
+
+    return "Hello from Flask.....@Mohibullah Kamal"
