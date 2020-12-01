c6541ff455efb4a58adffc3eeec934dc3fd0f551
mogit2020 <mohibullah.kamal@gmail.com>
2020-12-01 18:39:49 +0100

    added navbar component

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
-    TodoItemComponent
+    TodoItemComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
